53f5adb788d2a49d9efaf55d84a12956063ff3fe
Jesus M. Castagnetto <jesus@upch.pe>
2014-07-21 15:09:46 -0500

    changes to plot 6

--- plot6.R
+++ plot6.R
@@ -1,7 +1,7 @@
-plot 6
+# plot 6
 # Compare emissions from motor vehicle sources in Baltimore City with
 # emissions from motor vehicle sources in Los Angeles County, California
-# ( fips == "06037" ). Which city has seen greater changes over time in
+# ( fips == "06037" ). Which City has seen greater changes over time in
 # motor vehicle emissions?
 
 NEI <- readRDS("summarySCC_PM25.rds")
@@ -17,30 +17,49 @@ emissions <- aggregate(x=cities_vehicles$Emissions,
                        FUN=sum)
 
 la <- emissions[emissions$fips=="06037",]
-la$city <- "Los Angeles, CA"
+la$City <- "Los Angeles, CA"
 la_1999 <- la[la$year==1999,]$x
 la$diff <- la$x - la_1999
 la$pchng <- 100 * la$diff / la_1999
+
 bal <- emissions[emissions$fips=="24510",]
-bal$city <- "Baltimore City, MD"
+bal$City <- "Baltimore City, MD"
 bal_1999 <- bal[bal$year==1999,]$x
 bal$diff <- bal$x - bal_1999
-bal$pchng <- 100* bal$diff / bal_1999
+bal$pchng <- 100 * bal$diff / bal_1999
+
 dat <- rbind(la, bal)
 
 library(ggplot2)
-p1 <- ggplot(dat, aes(y=x, x=year, group=city, color=city))
-p1 + geom_point(size=3) + geom_line() +
+p1 <- ggplot(dat, aes(y=x, x=year, group=City, color=City))
+p1 <- p1 + geom_point(size=3) + geom_line() +
     xlab("Year") + ylab("Total Emissions (tons)") +
-    ggtitle(expression("PM"[2.5]*" Vehicle Emissions (comparison)")) +
+    ggtitle("Yearly Emissions") +
+    theme_bw() + theme(legend.position="none")
+p2 <- ggplot(dat, aes(y=diff, x=year, group=City, color=City))
+p2 <- p2 + geom_point(size=3) + geom_line() +
+    xlab("Year") + ylab("Difference of\nEmissions (tons)") +
+    ggtitle("Difference (ref: 1999)") +
+    theme_bw() + theme(legend.position="none")
+p3 <- ggplot(dat, aes(y=pchng, x=year, group=City, color=City))
+p3 <- p3 + geom_point(size=3) + geom_line() +
+    xlab("Year") + ylab("Percent change") +
+    ylim(-80, 30) +
+    ggtitle("Percent Change (ref: 1999)") +
     theme_bw()
-p2 <- ggplot(dat, aes(y=diff, x=year, group=city, color=city))
-p2 + geom_point(size=3) + geom_line() +
-    xlab("Year") + ylab("Total Emissions (tons)") +
-    ggtitle(expression("PM"[2.5]*" Vehicle Emissions (comparison)")) +
-    theme_bw()
-p3 <- ggplot(dat, aes(y=pchng, x=year, group=city, color=city))
-p3 + geom_point(size=3) + geom_line() +
-    xlab("Year") + ylab("Percent change (ref: 1999 emissions)") +
-    ggtitle(expression("PM"[2.5]*" Emissions by City (comparison)")) +
-    theme_bw()
+
+g_legend<-function(a.gplot){
+    tmp <- ggplot_gtable(ggplot_build(a.gplot))
+    leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+    legend <- tmp$grobs[[leg]]
+    legend
+}
+
+legend <- g_legend(p3)
+
+library(gridExtra)
+grid.arrange(p1,p2,p3 + theme(legend.position="none"), legend,
+             main=textGrob(expression("PM"[2.5]*" Emissions by City")),
+             ncol=2)
+
+
